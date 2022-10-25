51abec767f384da25674180b812666f21e3ff6f0
a4lab2 <a4lab2@gmail.com>
2022-10-25 20:55:39 +0100

    Utilities added to handle json and redirect

--- main.go
+++ main.go
@@ -1,10 +1,20 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
+	"io"
 	"log"
 	"net/http"
+	"net/http/httputil"
+	"net/url"
 	"os"
+	"strings"
 )
+
+type requestPayloadStruct struct {
+	ProxyCondition string `json:"proxy_condition"`
+}
 
 func getEnv(key, fallback string) string {
 	if value, ok := os.LookupEnv(key); ok {
@@ -29,7 +39,34 @@ func logSetup() {
 	log.Printf("Redirecting to Default url: %s\n", default_condtion_url)
 }
 func handleRequestAndRedirect(w http.ResponseWriter, r *http.Request) {
+	requestPayload := parseRequestBody(r)
 
+	url := getProxyUrl(requestPayload.ProxyCondition)
+	logRequestPayload(requestPayload, url)
+	serveReverseProxy(url, w, r)
+}
+
+func logRequestPayload(requestionPayload requestPayloadStruct, proxyUrl string) {
+	log.Printf("proxy_condition: %s, proxy_url: %s\n", requestionPayload.ProxyCondition, proxyUrl)
+}
+
+func getProxyUrl(proxyConditionRaw string) string {
+	proxyCond := strings.ToUpper(proxyConditionRaw)
+
+	a_condtion_url := os.Getenv("A_CONDITION_URL")
+	b_condtion_url := os.Getenv("B_CONDITION_URL")
+	default_condtion_url := os.Getenv("DEFAULT_CONDITION_URL")
+
+	switch proxyCond {
+	case "A":
+		return a_condtion_url
+
+	case "B":
+		return b_condtion_url
+
+	default:
+		return default_condtion_url
+	}
 }
 func main() {
 	logSetup()
@@ -38,4 +75,36 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+}
+
+func requestBodyDecoder(r *http.Request) *json.Decoder {
+	b, err := io.ReadAll(r.Body)
+	if err != nil {
+
+		panic(err)
+	}
+
+	r.Body = io.NopCloser(bytes.NewBuffer(b))
+	return json.NewDecoder(io.NopCloser(bytes.NewBuffer(b)))
+}
+
+func parseRequestBody(r *http.Request) requestPayloadStruct {
+	decoder := requestBodyDecoder(r)
+	var requestPayload requestPayloadStruct
+	err := decoder.Decode(&requestPayload)
+	if err != nil {
+		panic(err)
+	}
+	return requestPayload
+
+}
+
+func serveReverseProxy(target string, w http.ResponseWriter, r *http.Request) {
+	url, _ := url.Parse(target)
+	proxy := httputil.NewSingleHostReverseProxy(url)
+	r.URL.Host = url.Host
+	r.URL.Scheme = url.Scheme
+	r.Header.Set("X-Forwarded-Host", r.Header.Get("Host"))
+	r.Host = url.Host
+	proxy.ServeHTTP(w, r)
 }
